92fedc556810f3446cade36695f0ee10e81e6ca6
WVista709 <massanitatikawa@gmail.com>
2024-11-22 19:09:34 -0400

    Add files via upload

---
+++ Solução de menos tres/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro{
+    class Program{
+        static void Main(String[] args){
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            
+            //Variaveis
+            int n1, n2, n3;
+
+            //Digite os valores
+            Console.Write("Digite o valores: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+
+            //Descobrindo quem é o menor
+            if (n1 < n2 && n1 < n3){
+                Console.WriteLine("Menor = " + n1);
+            } else if (n2 < n1 && n2 < n3){
+                Console.WriteLine("Menor = " + n2);
+            } else if (n3 < n1 && n3 < n2){
+                Console.WriteLine("Menor = " + n3);
+            }
+        }
+    }
+}
---
+++ Solução de problema crescente/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primeiro{
+    class Program{
+        static void Main(string[] args){
+            //Variaveis
+            int X, Y;
+
+            //Digite o valor
+            Console.Write("Digite o valor de X: ");
+            X = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            Y = int.Parse(Console.ReadLine());
+
+            //Descobrindo quem é o menor
+            if (X > Y){
+                Console.WriteLine("DECRESCENTE!");
+            } else if (X < Y){
+                Console.WriteLine("CRESCENTE!");
+            }
+        }
+    }
+}
